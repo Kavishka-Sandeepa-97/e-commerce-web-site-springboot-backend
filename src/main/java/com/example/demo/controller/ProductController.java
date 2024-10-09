6c2553c798f3fc70d816c9c10a8c53db64b799a2
Kavishka Sandeepa <kavishkapk99@gmail.com>
2024-10-09 21:45:01 +0530

    empty array return handle

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -43,8 +43,13 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(product);
     }
     @GetMapping("productByCategory")
-    public ResponseEntity<List<Product>> getProductByCategory(@RequestBody Category category){
-       return ResponseEntity.status(HttpStatus.OK).body( productService.getProductByCategory(category));
+    public ResponseEntity<?> getProductByCategory(@RequestBody Category category){
+        if (productService.getProductByCategory(category).isEmpty()){
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found for id="+category.getId());
+        }else {
+            return ResponseEntity.status(HttpStatus.OK).body(productService.getProductByCategory(category));
+        }
+
     }
 
 }
